e435130dcd52a7b71f8a895bdf8be824f85666ef
Hasan-E <hasan.erhan1302@gmail.com>
2025-07-12 01:11:52 +0300

    import delete

--- PIXIM/src/components/PiximDrawer.jsx
+++ PIXIM/src/components/PiximDrawer.jsx
@@ -6,8 +6,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import MuiDrawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import { styled, useTheme } from "@mui/material/styles";
